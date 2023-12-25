a01d12a1c49967dd626a8357627f2cd557c83f47
fahim <fahimahamedweb@gmail.com>
2023-12-25 22:39:11 +0600

    added helper

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -1,24 +1,40 @@
 <?php
 
 namespace Database\Seeders;
-
+use Spatie\Permission\Models\Permission;
 use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
+use DB;
 class AdminSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds.php artisan db:seed --class=AdminSeeder
      */
     public function run(): void
     {
         $admin = new Admin();
         $admin->image ='/test';
-        $admin->name='Super Test';
-        $admin->email='admin@gmail.com';
-        $admin->password="$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";//password
+        $admin->name='Super Admin';
+        $admin->email='admin@admin.com';
+        $admin->password=Hash::make('admin');//password
         $admin->status =1;
+        
         $admin->save();
+        \DB::table('roles')->insert(
+            [
+                ['id' => 4, 'name' => 'Super Admin', 'guard_name' => 'admin'],
+                
+            ]
+        );
+        \DB::table('model_has_roles')->insert(
+            [
+                ['role_id' => 4, 'model_type' => 'App\Models\Admin', 'model_id' => '1'],
+                
+            ]
+        );
+        
+       
     }
 }
